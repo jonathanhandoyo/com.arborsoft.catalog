23bb2b5662438be1daca42adaa63376319b13df8
Jonathan Handoyo <jonathan.handoyo@gmail.com>
2015-11-08 21:04:40 +0800

    enabling neo4jtemplate and fixing slf4j duplication

--- src/main/java/com/arborsoft/catalog/config/ApplicationConfiguration.java
+++ src/main/java/com/arborsoft/catalog/config/ApplicationConfiguration.java
@@ -4,11 +4,15 @@ import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.server.Neo4jServer;
 import org.springframework.data.neo4j.server.RemoteServer;
+import org.springframework.data.neo4j.template.Neo4jOperations;
+import org.springframework.data.neo4j.template.Neo4jTemplate;
 
 @Configuration
 @ComponentScan(basePackages = {"com.arborsoft.catalog"})
@@ -18,11 +22,26 @@ public class ApplicationConfiguration extends Neo4jConfiguration {
 
     @Bean
     public Neo4jServer neo4jServer() {
-        return new RemoteServer("http://jonathantest2:yG3vVKGa5kteDoX2h5sH@jonathantest2.sb02.stations.graphenedb.com:24789");
+        String url = "http://jonathantest2:yG3vVKGa5kteDoX2h5sH@jonathantest2.sb02.stations.graphenedb.com:24789";
+        LOG.info("neo4j >> " + url);
+        return new RemoteServer(url);
     }
 
     @Bean
     public SessionFactory getSessionFactory() {
-        return new SessionFactory("com.arborsoft.catalog.model");
+        String _package = "com.arborsoft.catalog.model";
+        LOG.info("neo4j >> models @ " + _package);
+        return new SessionFactory(_package);
+    }
+
+    @Bean
+    @Override
+    public Session getSession() throws Exception {
+        return super.getSession();
+    }
+
+    @Bean
+    public Neo4jOperations neo4jTemplate() throws Exception {
+        return new Neo4jTemplate(this.getSession());
     }
 }
--- src/test/java/com/arborsoft/catalog/test/MainTest.java
+++ src/test/java/com/arborsoft/catalog/test/MainTest.java
@@ -1,12 +1,14 @@
 package com.arborsoft.catalog.test;
 
 import com.arborsoft.catalog.config.ApplicationConfiguration;
+import com.arborsoft.catalog.model.Actor;
 import com.arborsoft.catalog.repository.ActorRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.template.Neo4jOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,9 +20,29 @@ public class MainTest {
     @Autowired
     private ActorRepository actorRepository;
 
+    @Autowired
+    private Neo4jOperations template;
+
     @Test
     public void test1() throws Exception {
-        this.actorRepository.friend(this.actorRepository.findByName("name1"), this.actorRepository.findByName("name2"));
-        this.actorRepository.unfriend(this.actorRepository.findByName("name2"), this.actorRepository.findByName("name1"));
+//        for (int i = 0; i < 100; i++) {
+            this.actorRepository.friend(this.actorRepository.findByName("name1"), this.actorRepository.findByName("name2"));
+//            this.actorRepository.unfriend(this.actorRepository.findByName("name2"), this.actorRepository.findByName("name1"));
+//        }
+
+        Actor test = this.template.load(Actor.class, 1L, 1);
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
+        LOG.info(test.toString());
     }
 }
